fa526f27d146e40d7167d03c34b04f8344de16c4
someron <devsomeron@gmail.com>
2024-09-06 23:47:57 +0200

    feat: Add home page for app

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,4 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Hello, {user}!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <a href="/test">Test page</a>
+
+<script lang="ts">
+    import { getCurrentUser } from "$lib/authentication";
+
+    let model = getCurrentUser();
+    const user = model == null ? "" : model.name;
+</script>
